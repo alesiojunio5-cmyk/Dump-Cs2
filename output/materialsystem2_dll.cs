e06ecd1e62918a9d3d14df3779c03a90d190cf93
alesiojunio5-cmyk <alesiojunio5@gmail.com>
2025-09-18 18:26:54 -0300

    fggg
    vv

--- output/materialsystem2_dll.cs
+++ output/materialsystem2_dll.cs
@@ -1,5 +1,5 @@
 // Generated using https://github.com/a2x/cs2-dumper
-// 2025-09-17 00:41:33.396903400 UTC
+// 2025-09-17 23:35:16.178232700 UTC
 
 namespace CS2Dumper.Schemas {
     // Module: materialsystem2.dll
