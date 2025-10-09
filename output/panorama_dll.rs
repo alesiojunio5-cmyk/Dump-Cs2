afd9ba20ff5f21036f6a963941f268c1b44e6f5f
alesiojunio5-cmyk <alesiojunio5@gmail.com>
2025-10-09 13:20:46 -0300

    xx

--- output/panorama_dll.rs
+++ output/panorama_dll.rs
@@ -1,5 +1,5 @@
 // Generated using https://github.com/a2x/cs2-dumper
-// 2025-10-04 12:39:31.642108200 UTC
+// 2025-10-07 00:00:22.635552200 UTC
 
 #![allow(non_upper_case_globals, non_camel_case_types, non_snake_case, unused)]
 
--- output/schemasystem_dll.cs
+++ output/schemasystem_dll.cs
@@ -1,5 +1,5 @@
 // Generated using https://github.com/a2x/cs2-dumper
-// 2025-10-04 12:39:31.642108200 UTC
+// 2025-10-07 00:00:22.635552200 UTC
 
 namespace CS2Dumper.Schemas {
     // Module: schemasystem.dll
