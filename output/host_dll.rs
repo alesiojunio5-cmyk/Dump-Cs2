03fe9d63fc896fd465abca5324189ecb991765e4
alesiojunio5-cmyk <alesiojunio5@gmail.com>
2025-10-15 12:23:48 -0300

    xxxx
    xxxx

--- output/host_dll.rs
+++ output/host_dll.rs
@@ -1,5 +1,5 @@
 // Generated using https://github.com/a2x/cs2-dumper
-// 2025-10-11 19:10:32.080067400 UTC
+// 2025-10-15 15:23:28.080299300 UTC
 
 #![allow(non_upper_case_globals, non_camel_case_types, non_snake_case, unused)]
 
