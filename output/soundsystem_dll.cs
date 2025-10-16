76a7ed09c87f3a050827dbdfd2a77e4fa0f1822c
alesiojunio5-cmyk <alesiojunio5@gmail.com>
2025-10-16 16:54:34 -0300

    xxxxxxxxx

--- output/soundsystem_dll.cs
+++ output/soundsystem_dll.cs
@@ -1,5 +1,5 @@
 // Generated using https://github.com/a2x/cs2-dumper
-// 2025-10-16 18:22:39.597781300 UTC
+// 2025-10-16 19:54:05.463921400 UTC
 
 namespace CS2Dumper.Schemas {
     // Module: soundsystem.dll
