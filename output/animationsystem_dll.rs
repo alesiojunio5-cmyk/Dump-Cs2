303da850c90f1c6d82ac2ec21fe3f08b39dd307f
alesiojunio5-cmyk <alesiojunio5@gmail.com>
2025-11-16 15:25:28 -0300

    lç~]

--- output/animationsystem_dll.rs
+++ output/animationsystem_dll.rs
@@ -1,5 +1,5 @@
 // Generated using https://github.com/a2x/cs2-dumper
-// 2025-11-08 22:20:27.901533400 UTC
+// 2025-11-16 18:25:16.116286900 UTC
 
 #![allow(non_upper_case_globals, non_camel_case_types, non_snake_case, unused)]
 
--- output/scenesystem_dll.hpp
+++ output/scenesystem_dll.hpp
@@ -1,5 +1,5 @@
 // Generated using https://github.com/a2x/cs2-dumper
-// 2025-11-08 22:20:27.901533400 UTC
+// 2025-11-16 18:25:16.116286900 UTC
 
 #pragma once
 
