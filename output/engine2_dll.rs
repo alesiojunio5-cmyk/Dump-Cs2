a08ffd729e01903b3e4ad83e9885f985c41a2dfc
alesiojunio5-cmyk <alesiojunio5@gmail.com>
2026-01-20 07:35:20 -0300

    xx
    xxx

--- output/engine2_dll.rs
+++ output/engine2_dll.rs
@@ -1,5 +1,5 @@
 // Generated using https://github.com/a2x/cs2-dumper
-// 2026-01-03 02:28:30.877587300 UTC
+// 2026-01-20 10:35:15.106728200 UTC
 
 #![allow(non_upper_case_globals, non_camel_case_types, non_snake_case, unused)]
 
--- output/particles_dll.cs
+++ output/particles_dll.cs
@@ -1,5 +1,5 @@
 // Generated using https://github.com/a2x/cs2-dumper
-// 2026-01-03 02:28:30.877587300 UTC
+// 2026-01-20 10:35:15.106728200 UTC
 
 namespace CS2Dumper.Schemas {
     // Module: particles.dll
