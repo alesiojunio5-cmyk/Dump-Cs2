dcad16c1a3fca3ba01efc187effe8093846073aa
alesiojunio5-cmyk <alesiojunio5@gmail.com>
2026-02-10 10:40:32 -0300

    asd

--- output/server_dll.cs
+++ output/server_dll.cs
@@ -1,5 +1,5 @@
 // Generated using https://github.com/a2x/cs2-dumper
-// 2026-02-08 15:40:24.067694500 UTC
+// 2026-02-10 13:40:17.627539600 UTC
 
 namespace CS2Dumper.Schemas {
     // Module: server.dll
--- output/soundsystem_dll.rs
+++ output/soundsystem_dll.rs
@@ -1,5 +1,5 @@
 // Generated using https://github.com/a2x/cs2-dumper
-// 2026-02-08 15:40:24.067694500 UTC
+// 2026-02-10 13:40:17.627539600 UTC
 
 #![allow(non_upper_case_globals, non_camel_case_types, non_snake_case, unused)]
 
